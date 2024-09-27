4a8995f625d50a05df4ba45f1aa77d782a838b43
Lazoangel <a.23308051281190@cbtis128.edu.mx>
2024-09-27 04:36:18 -0600

    Add files via upload

---
+++ Lazo_Angel_1190_3W_5.py
@@ -0,0 +1,8 @@
+print ("Lazo Jimenez Angel Jesus")
+valor_1 = float (input("ingresar primer valor: ")) #Pide al usuario ingresar el primero valor
+valor_2 = float (input("ingresar segundo valor: ")) #Pide al usuario ingresar el segundo valor
+numero_menor = min (valor_1,valor_2) #Esta linea compara los dos valores para ver cual es el menor
+print (f"el valor menor es: {numero_menor}") #Muestra el valor minimo entre los dos valores ingresados 
+print ("") #Esta linea es unicamente para crear un espacio
+suma = valor_1 + valor_2 #Se hace la suma de los valores
+print (f"la suma de {valor_1} y {valor_2} es: {suma}") #Se muestra el resultado de la suma de los valores
